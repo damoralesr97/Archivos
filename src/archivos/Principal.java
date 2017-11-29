6b489052453d49eb78394080036d36f59fa7cb49
Estudiante <Estudiante@lab0619>
2017-11-29 08:35:28 -0500

    Archivos

--- src/archivos/Principal.java
+++ src/archivos/Principal.java
@@ -18,19 +18,30 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        //crear directorio
         File raiz = new File("c:/raiz");
         if(raiz.exists()==false){
             raiz.mkdir();
         }
         
+        //crear 1000 directorios
         for(int i=0;i<=1000;i++){
             File directorio = new File("c:/raiz/directorio"+i);
             directorio.mkdir();
         }
         
+        //borrar 500 directorios
         for(int i=0;i<=500;i++){
             File directorio = new File("c:/raiz/directorio"+i);
             directorio.delete();
+        }
+        
+        //cambiar de nombre a 250 directorios
+        for(int i=501;i<=751;i++){
+            File directorio = new File("c:/raiz/directorio"+i);
+            File hola = new File("c:/raiz/hola"+i);
+            directorio.renameTo(hola);
         }
     }
     
